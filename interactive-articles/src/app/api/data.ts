1539bb1bbbabfec5f886c8dceb46d227d3136a31
YRosh <roshanreddyy@gmail.com>
2024-04-18 22:07:33 -0400

    Made data persist

--- interactive-articles/src/app/api/data.ts
+++ interactive-articles/src/app/api/data.ts
@@ -1,11 +1,24 @@
-// Volatile data storage to store the answers given by students for each question
+const fs = require("fs");
 
-let questions: { [id: string]: { question: string; answer: string } } = {};
-
-export function getQuestions() {
-   return questions;
+interface questions {
+   [id: string]: { question: string; answer: string };
 }
 
-export function updateQuestions(newQuestions: typeof questions) {
-   questions = newQuestions;
+export function getQuestions() {
+   try {
+      const data = fs.readFileSync("./src/app/api/data.json", "utf8");
+      return JSON.parse(data);
+   } catch (err) {
+      console.error("Error reading JSON file:", err);
+      return {};
+   }
+}
+
+export function updateQuestions(newQuestions: questions) {
+   try {
+      const jsonData = JSON.stringify(newQuestions, null, 2);
+      fs.writeFileSync("./src/app/api/data.json", jsonData);
+   } catch (err) {
+      console.error("Error writing JSON file:", err);
+   }
 }
